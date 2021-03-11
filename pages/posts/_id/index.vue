8f6e67ee6f3e8f7c91482c8658c16ab047ff14bd
Venturesome777 <orfeasnifakos95@gmail.com>
2021-03-11 21:50:17 +0200

    pages components

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="single-post-page">
+       <section class="post">
+          <h1>Title of the Posts</h1>
+          <div class="post-details">
+            <div>Last updated on XXX</div>
+            <div>Written by Name</div>
+          </div>
+          <p>Content of the Post</p>
+       </section>
+       <section class="post-feedback">
+           <p>Let me know what you think</p>
+       </section>
+    </div>
+</template>
